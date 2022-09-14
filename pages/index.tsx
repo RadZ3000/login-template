78b3edca0386b32e063d3528ae77209279d41ab2
Herald <herald.adowei@gmail.com>
2022-09-14 15:15:44 -0500

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         <div className="grid">
           <div>
             <h1 className="lg:text-4xl xl:text-8xl xl:leading-snug font-extrabold">Square.</h1>
-            <h1 className="lg:text-2xl xl:text-5xl xl:leading-snug font-extrabold">Shaping our future.</h1>
+            <h1 className="lg:text-2xl xl:text-5xl xl:leading-snug font-extrabold">Molding our future, one shape at a time</h1>
           </div>
           <div className="px-2">
             <p className="text-lg">Don't have an account?</p>
@@ -32,28 +32,28 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="grid gap-y-4 justify-center content-center text-center bg-gray-200 w-3/5">
+      <div className="grid gap-y-4 justify-center content-center text-center bg-slate-900 text-white w-3/5">
         <h1 className="text-4xl md:text-5xl font-bold mt-52">Login to your account</h1>
         <h3 className="text-md md:text-xl">Continue where you left off</h3>
-        <input type="text" placeholder="Username" className="flex border-1 border-black rounded-lg px-3 py-2" />
-        <input type="text" placeholder="Password" className="flex border-1 border-black rounded-lg px-3 py-2" />
-        <button className="bg-black text-white py-2 rounded-lg">Login with Email</button>
+        <input type="text" placeholder="Username" className="flex border-1 border-white rounded-lg px-3 py-2 text-black" />
+        <input type="text" placeholder="Password" className="flex border-1 border-white rounded-lg px-3 py-2 text-black" />
+        <button className="bg-slate-400 text-black py-2 rounded-lg hover:text-white">Login with Email</button>
         <div className="flex justify-center items-center">
-          <span className="w-full border border-black"></span>
+          <span className="w-full border border-white"></span>
           <span className="px-4">Or</span>
-          <span className="w-full border border-black"></span>
+          <span className="w-full border border-white"></span>
         </div>
-        <button className="flex items-center gap-x-36 bg-transparent text-black border-2 rounded-lg border-black py-2 px-4 ">
+        <button className="flex items-center gap-x-36 bg-transparent text-white border-2 rounded-lg border-white py-2 px-4 hover:text-red-300 hover:border-red-300">
           <FaGoogle className="text-sky-500 lg:text-xl xl:text-xl" />
           Login with Google
         </button>
-        <h3 className="mt-36">Built by <a href="#" className="underline">Herald Adowei</a></h3>
+        <h3 className="mt-36">Built by <a href="https://herald.vercel.app" target="_blank" rel="noopener noreferrer" className="underline hover:text-red-300">Herald Adowei</a></h3>
         <div className="flex flex-row gap-x-8 justify-center pt-2">
-          <a href="https://www.linkedin.com/in/heraldadowei/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="lg:text-xl xl:text-xl" /></a>
-          <a href="https://codepen.io/He_sRad/" target="_blank" rel="noopener noreferrer"><FaCodepen className="lg:text-xl xl:text-xl" /></a>
-          <a href="https://github.com/RadZ3000" target="_blank" rel="noopener noreferrer"><FaGithub className="lg:text-xl xl:text-xl" /></a>
-          <a href="https://www.instagram.com/herald_a/" target="_blank" rel="noopener noreferrer"><FaInstagram className="lg:text-xl xl:text-xl" /></a>
-          <a href="https://twitter.com/The_RadZee" target="_blank" rel="noopener noreferrer"><FaTwitter className="lg:text-xl xl:text-xl" /></a>
+          <a href="https://www.linkedin.com/in/heraldadowei/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="lg:text-xl xl:text-xl hover:text-red-300" /></a>
+          <a href="https://codepen.io/He_sRad/" target="_blank" rel="noopener noreferrer"><FaCodepen className="lg:text-xl xl:text-xl hover:text-red-300" /></a>
+          <a href="https://github.com/RadZ3000" target="_blank" rel="noopener noreferrer"><FaGithub className="lg:text-xl xl:text-xl hover:text-red-300" /></a>
+          <a href="https://www.instagram.com/herald_a/" target="_blank" rel="noopener noreferrer"><FaInstagram className="lg:text-xl xl:text-xl hover:text-red-300" /></a>
+          <a href="https://twitter.com/The_RadZee" target="_blank" rel="noopener noreferrer"><FaTwitter className="lg:text-xl xl:text-xl hover:text-red-300" /></a>
         </div>
       </div>
 
